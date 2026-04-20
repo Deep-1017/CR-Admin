75087eff6adba3f1637edf48b107ae43109d53de
DeepKansara1212 <deepkansara1212@gmail.com>
2026-04-20 23:16:32 +0530

    Updated Admin Functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Eye, EyeOff, Music2, Lock } from "lucide-react";
 
-const API_URL = "http://localhost:5000/api/v1";
+const API_URL = "http://localhost:5000/api";
 
 interface LoginProps {
   onLogin: () => void;
